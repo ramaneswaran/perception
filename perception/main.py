f17d7d61735ca3b7711415d30cc612c726ea6308
ramaneswaran <ramannewdelhi44@gmail.com>
2021-03-16 01:40:16 +0530

    Add routes for insertion and search

--- perception/main.py
+++ perception/main.py
@@ -1,15 +1,36 @@
+import os
 from typing import List
 
+import numpy as np
+
 from fastapi import Depends, FastAPI, HTTPException
+from fastapi import File, Body, Response
+
+from pydantic import BaseSettings
+
+import uvicorn
+
 from sqlalchemy.orm import Session
 
 from perception import crud, models, schemas
 from perception.database import SessionLocal, engine
+from perception.core.faiss_helper import FaissCore
 
 models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
 
+
+class Settings(BaseSettings):
+    app_name: str = "Perception"
+    base_dir: str = os.path.dirname(os.path.realpath(__file__))
+    index_store_path: str = os.path.join(base_dir, "index_store")
+    index_name: str = "vector.index"
+
+
+settings = Settings()
+
+app = FastAPI()
+index = FaissCore(name=settings.index_name, store=settings.index_store_path, dimension=6)
 
 # Dependency
 def get_db():
@@ -20,10 +41,25 @@ def get_db():
         db.close()
 
 
-@app.post("/food/", response_model=schemas.Food)
-def create_user(food: schemas.FoodCreate, db: Session = Depends(get_db)):
+@app.post("/food/insert", response_model=schemas.Food)
+def create_food(food: schemas.FoodCreate, index_file: str = Body(...), dimension: int = Body(...), db: Session = Depends(get_db)):
     
-    # if db_user:
-    #     raise HTTPException(status_code=400, detail="Email already registered")
+
+    vector = np.array(food.embedding)
+
+    index_id = index.insert(vector)
+
+    return crud.create_food(db=db, food=food, index_id=index_id)
+
+
+@app.post("/food/search",response_model=schemas.SearchResult)
+def search_food(query: List[float] = Body(...), index_file: str = Body(...), dimension: int = Body(...), db: Session = Depends(get_db)):
     
-    return crud.create_food(db=db, food=food)
+    vector = np.array(query)
+
+
+    index_ids = index.search(vector,4)
+
+    result = schemas.SearchResult(index_ids=index_ids[0].tolist())
+
+    return result
