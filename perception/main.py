242a1b5a4f4fb34bb88b5a868ad3b84182556a79
ramaneswaran <ramannewdelhi44@gmail.com>
2021-03-25 00:47:25 +0530

    Add search by image endpoint

--- perception/main.py
+++ perception/main.py
@@ -2,10 +2,14 @@ import os
 import yaml
 from typing import List
 
+from PIL import Image
+import io
+
 import numpy as np
+import shutil
 
 from fastapi import Depends, FastAPI, HTTPException
-from fastapi import File, Body, Response
+from fastapi import File, Body, Response, File, UploadFile
 
 from pydantic import BaseSettings
 
@@ -16,6 +20,8 @@ from sqlalchemy.orm import Session
 from perception import crud, models, schemas
 from perception.database import SessionLocal, engine
 from perception.core.faiss_helper import FaissCore
+
+from perception.encoders.conv import VGGEncoder
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -37,6 +43,7 @@ settings = Settings()
 
 app = FastAPI()
 index = FaissCore(name=settings.index_name, store=settings.index_store_path, dimension=settings.dimension)
+encoder = VGGEncoder()
 
 # Dependency
 def get_db():
@@ -80,3 +87,35 @@ def search_food(query: schemas.SearchQuery,  db: Session = Depends(get_db)):
 
     return result
 
+@app.post("/food/image", response_model=schemas.SearchResult)
+async def search_with_image(image: UploadFile = File(...), db: Session = Depends(get_db)):
+    # data = await image.read()
+    # byte_data = io.BytesIO(data)
+    # image = Image.open(io.BytesIO(data))
+
+    # f = open("log.txt", mode="w")
+    # f.write(str(type(data))+"\n")
+    # f.write(str(type(image.file))+"\n")
+    # f.write(str(type(byte_data)))
+    # f.close()
+
+    with open("out.png", "wb") as buffer:
+        shutil.copyfileobj(image.file, buffer)
+    
+    image_data = Image.open("out.png").convert("RGB")
+    feature = encoder.encode(image_data)
+
+
+    index_ids = index.search(feature,4)
+    food_items = []
+    for index_id in index_ids[0]:
+        
+        if index_id != -1:
+            item = crud.get_food_by_index_id(db,index_id)
+            if item is not None:
+              
+                food_items.append(schemas.Food.from_orm(item))
+
+    result = schemas.SearchResult(result=food_items, index_ids=index_ids[0].tolist())
+
+    return result
